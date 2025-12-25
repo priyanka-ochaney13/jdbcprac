0c22abba20878796bd625f241b91754bbda0c78e
priyanka-ochaney13 <priyanka.ochaney@gmail.com>
2025-12-25 14:27:11 +0530

    smtg

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,30 +1,31 @@
 package com.example;
 import java.lang.Thread.State;
 import java.sql.*;
-
+//ResultSet rs = st.executeQuery(query);
+//rs.next(); pointing to first row and checks if there is data in the next row or not
+//it will return false for an insert query as there is no data to be returned
+//for select query it will return true as there is data to be returned
+//execute method can be used for any type of query but executeQuery is only for select queries (it returns ResultSet) and execute returns any type of result (boolean)
 
 public class App 
 {
     public static void main( String[] args ) throws Exception {
 
-        String url = "jdbc:postgresql://localhost:5432/demo";
-        String uname = "postgres";
-        String pwd = "";
+        
 
-        String query = "delete from student where sid=3";
+        String query = "insert into student values (?, ?, ?)";
         
         Class.forName("org.postgresql.Driver");
         
         Connection con = DriverManager.getConnection(url, uname, pwd);
         System.out.println("Connection Successful");
 
-        Statement st = con.createStatement();
-        //ResultSet rs = st.executeQuery(query);
-        //rs.next(); pointing to first row and checks if there is data in the next row or not
-        st.execute(query);
-        //it will return false for an insert query as there is no data to be returned
-        //for select query it will return true as there is data to be returned
-        //execute method can be used for any type of query but executeQuery is only for select queries (it returns ResultSet) and execute returns any type of result (boolean)
+        PreparedStatement st = con.prepareStatement(query);
+        st.setInt(1, sid);
+        st.setString(2, sname);
+        st.setInt(3, marks);
+        st.execute();
+        
         con.close();
     }
 }
